16416ba4a87b39edb234fd06dc21ff4707bcc693
Barry Burke <SANdood@users.noreply.github.com>
2016-10-18 12:44:31 -0400

    Update Spruce Scheduler.groovy

--- Spruce Scheduler.groovy
+++ Spruce Scheduler.groovy
@@ -980,10 +980,11 @@ private def resetEverything() {
 
 // unsubscribe from ALL events EXCEPT app.touch
 private def unsubAllBut() {
+	unsubscribe()
 	unsubscribe(settings.switches)
 	unsubWaterStoppers()
 	if (settings.sync) unsubscribe(settings.sync)
-
+	subscribe(app, appTouch)
 }
 
 // enable the "Play" button in SmartApp list
